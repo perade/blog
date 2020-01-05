e3b3230c50d6d101e47376b414e22742fd278d80
perade <perade0530@gmail.com>
2020-01-06 02:22:14 +0900

    change about author

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,20 +1,22 @@
 <template>
 	<div class="author">
 
-		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
+		<g-image alt="Author image" class="author__image" src="~/assets/images/Author.jpg" width="180" height="180" blur="5" />
 
 		<h1 v-if="showTitle" class="author__site-title">
 			{{ $static.metadata.siteName }}
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
+			<!-- A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content. -->
+			{{ $static.metadata.siteDescription }}
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<!-- <a href="//twitter.com/gridsome">Follow on Twitter</a>
+			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a> -->
 		</p>
+
 
 	</div>
 </template>
@@ -23,6 +25,7 @@
 query {
   metadata {
     siteName
+		siteDescription
   }
 }
 </static-query>
